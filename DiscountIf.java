3909ba78616ff6099811c7cb6fbbe2a9dd62ab2b
Dhanamjai <dhanamjaiketham@gmail.com>
2023-11-06 12:56:24 +0530

    fordiff

---
+++ DiscountIf.java
@@ -0,0 +1,38 @@
+public class Discount
+{
+	public static void main(String args[])
+	{
+		int price = 15000;
+		double product_discount,final_price;
+		if(price >10000)
+		{
+			product_discount = 50/100.0*price;
+			final_price = price - product_discount;
+			System.out.println("Eligible to dicount of 50% and your final price is "+final_price);
+		}
+		else if(price >8000)
+		{
+			product_discount = 30/100.0*price;
+			final_price = price - product_discount;
+			System.out.println("Eligible to dicount of 30% and your final price is "+final_price);
+		}
+		else if(price >2000)
+		{
+			product_discount = 10/100.0*price;
+			final_price = price - product_discount;
+			System.out.println("Eligible to dicount of 10% and your final price is "+final_price);
+		}
+		else if(price >1000)
+		{
+			product_discount = 5/100.0*price;
+			final_price = price - product_discount;
+			System.out.println("Eligible to dicount of 5% and your final price is "+final_price);
+		}
+		else 
+		{
+			
+			System.out.println("you are not Eligible to dicount buy anything above 1000 rupees ");
+		}
+		
+	}
+}
